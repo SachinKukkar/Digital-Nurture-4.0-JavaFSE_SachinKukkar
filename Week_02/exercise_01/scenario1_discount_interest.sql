eb4ac548647f44a99e4fd7646f564b4c6ec1cb78
Sachin Kukkar <Sachinkukkar1998@gmail.com>
2025-06-26 23:03:20 +0530

    Week 02 Done - PL/SQL

---
+++ Week_02/exercise_01/scenario1_discount_interest.sql
@@ -0,0 +1,23 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    CURSOR cust_cursor IS
+        SELECT CustomerID, DOB FROM Customers;
+
+    v_age NUMBER;
+BEGIN
+    FOR cust_rec IN cust_cursor LOOP
+        -- Calculate age
+        v_age := FLOOR(MONTHS_BETWEEN(SYSDATE, cust_rec.DOB) / 12);
+
+        IF v_age > 60 THEN
+            -- Apply 1% discount to all loans of this customer
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = cust_rec.CustomerID;
+
+            DBMS_OUTPUT.PUT_LINE('Discount applied to Customer ID ' || cust_rec.CustomerID || ' (Age: ' || v_age || ')');
+        END IF;
+    END LOOP;
+END;
+/
